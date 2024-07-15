de299befa363e190b1b9ccf6862e0514f872692b
Germán Riveros <gariverosl23@gmail.com>
2024-07-14 22:31:36 -0400

    Commit TR

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -20,4 +20,29 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+//TODO:Aislado! 
+it('Debe de existir el AppComponent', () => {
+  const fixture = TestBed.createComponent(LoginComponent);
+  const app = fixture.componentInstance
+  expect(app).toBeTruthy(); //TODO: ✔
+});
+
+it('Debe retornar formulario valido', () => {
+  const fixture = TestBed.createComponent(LoginComponent);
+  const app = fixture.componentInstance
+  fixture.detectChanges()
+
+  let email = app.loginform.controls['email']
+  let password = app.loginform.controls['password']
+
+
+  email.setValue('test1')
+  password.setValue('test1')
+
+
+
+  expect(app.loginform.invalid).toBeFalse(); //TODO: ✔
+});
+
 });
